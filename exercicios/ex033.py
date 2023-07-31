d5e3f4edaab686962160e140b66a46843f8c63a8
Laura Lima <laura.lima@escolar.ifrn.edu.br>
2023-07-31 09:44:25 -0300

    feat: exercício 33

---
+++ exercicios/ex033.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+n3 = int(input("Digite o terceiro número: "))
+lista = [n1, n2, n3]
+lista.sort()
+print(f"O menor número é {lista[0]}")
+print(f"O maior valor é {lista[2]}")
