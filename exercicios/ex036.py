9f2a93aa597590de4bd9294b9ee48e45a062e78d
Laura Lima <laura.lima@escolar.ifrn.edu.br>
2023-08-01 10:03:15 -0300

    fiz: correção de calculo da prestação

--- exercicios/ex036.py
+++ exercicios/ex036.py
@@ -1,9 +1,9 @@
 valor_casa = float(input("Informe o valor da casa: R$"))
-salario = float(input("Inform o valor do seu salário: "))
+salario = float(input("Inform o valor do seu salário: R$"))
 tempo = float(input("Em quantos anos você deseja pagar a casa? "))
 prestacao = valor_casa / (tempo*12) 
-teto = salario - (30/100 * salario) 
+teto = (30/100 * salario) 
 if prestacao > teto:
     print("Empréstimo negado! O valor da prestação não pode exceder 30% do seu salário.")
 else:
-    print("Empréstimo aprovado!")
+    print(f"Empréstimo aprovado! O valor da sua prestação mensal será de R${prestacao:.2f}")
