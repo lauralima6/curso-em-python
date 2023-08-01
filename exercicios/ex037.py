554bc202f4baeddd5991d1fcddcd8790b6d72f0c
Laura Lima <laura.lima@escolar.ifrn.edu.br>
2023-08-01 11:07:29 -0300

    feat: adicionando a opção de recomeçar a operação no menu de opções

--- exercicios/ex037.py
+++ exercicios/ex037.py
@@ -1,9 +1,17 @@
+import time
+
 n = int(input('Digite um número inteiro: '))
-print("MENU DE OPÇÕES \n Escolha uma das opções abaixo para ser a base da conversão: \n 1 - BINÁRIO \n 2 -OCTAL \n 3 - HEXADECIMAL")
-opcao = int(input("Sua escolha: "))
-if opcao == 1: 
-    print("{} convertido para binário é {:b}".format(n, n)) 
-if opcao == 2:
-    print("{} convertido para octal é {:o}".format(n, n))
-if opcao == 3:
-    print("{} convertido para hexadecimal é {}".format(n, hex(n)))
+def main ():
+    print("MENU DE OPÇÕES \n Escolha uma das opções abaixo para ser a base da conversão: \n 1 - BINÁRIO \n 2 -OCTAL \n 3 - HEXADECIMAL")
+    opcao = int(input("Sua escolha: "))
+    if opcao == 1: 
+        print("{} convertido para binário é {:b}".format(n, n)) 
+    if opcao == 2:
+        print("{} convertido para octal é {:o}".format(n, n))
+    if opcao == 3:
+        print("{} convertido para hexadecimal é {}".format(n, hex(n)))
+    else: 
+      print("Opção inválida, tente novamente!")
+      time.sleep(1.5)
+      main()
+main()
